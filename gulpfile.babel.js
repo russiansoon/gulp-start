0862b10ad130d5591d82c39d4772f69cca86adbc
Shevchenko Nik <russiansoon@gmail.com>
2016-02-20 14:58:33 +0300

    New production env

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,8 @@
 'use strict';
 
 import requireDir from 'require-dir';
+import gutil from 'gulp-util';
+
+var isProduction = (gutil.env.production === true ? true : false);
 
 requireDir('./lib/gulp/tasks', { recurse: true });
